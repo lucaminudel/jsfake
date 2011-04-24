d46025b604f3689d3910f3fc1411cbf8d2be392e
lucaminudel <luca@minudel>
2011-04-24 19:49:17 +0200

    Added tests for integration with Jasmine

--- specs/jsfake.mock.specs.js
+++ specs/jsfake.mock.specs.js
@@ -1,16 +1,16 @@
-describe('jsFake mock', function(){
+describe('jsFake mock', function () {
 	function SomeClass() {
 		var self = this;
 		self.privilegedMethod = function () { throw 'privilegedMethod'; };
 	}
 	SomeClass.prototype = {
-		protoMethodWithoutArgs: function(){ throw 'protoMethodWithoutArgs'; },
-		protoMethodWithArgs: function(arg1, arg2){ throw 'protoMethodWithArgs'; },
-		_privateMethod : function(){ throw '_privateMethod'; }
+		protoMethodWithoutArgs: function () { throw 'protoMethodWithoutArgs'; },
+		protoMethodWithArgs: function (arg1, arg2) { throw 'protoMethodWithArgs'; },
+		_privateMethod: function () { throw '_privateMethod'; }
 	};
 	var blueprint = new SomeClass();
-	blueprint.instMethodWithoutArgs = function(){ throw 'instMethodWithoutArgs'; };
-	blueprint.instMethodWithArgs = function(arg1, arg2){ throw 'instMethodWithArgs'; };
+	blueprint.instMethodWithoutArgs = function () { throw 'instMethodWithoutArgs'; };
+	blueprint.instMethodWithArgs = function (arg1, arg2) { throw 'instMethodWithArgs'; };
 	blueprint._privateMethod = function () { throw '_privateMethod'; };
 
 	function SomeDerivedClass() {
@@ -22,36 +22,36 @@ describe('jsFake mock', function(){
 	var derivedClassBlueprint = new SomeDerivedClass();
 	derivedClassBlueprint.instMethodOfDerivedClass = function () { throw 'instMethodOfDerivedClass'; };
 
-	it('should create a new instance from blueprint', function(){
+	it('should create a new instance from blueprint', function () {
 		expect(a.mock(SomeClass)).not.toEqual(undefined);
 		expect(a.mock(blueprint)).not.toBe(blueprint);
 	});
-  
-	it('should provide default unexpected-call behavior for all "public" prototype methods', function(){
-		var mock = a.mock(SomeClass);		
-		expect(function(){mock.protoMethodWithoutArgs();}).toThrow();
-		expect(function(){mock.protoMethodWithArgs(1,2);}).toThrow();
+
+	it('should provide default unexpected-call behavior for all "public" prototype methods', function () {
+		var mock = a.mock(SomeClass);
+		expect(function () { mock.protoMethodWithoutArgs(); }).toThrow();
+		expect(function () { mock.protoMethodWithArgs(1, 2); }).toThrow();
 		expect(mock._privateMethod).toBe(undefined);
 	});
 
 	it('should provide default unexpected-call behavior for all "public" prototype methods of a derived class', function () {
 		var mock = a.mock(SomeDerivedClass);
-		expect(function() { mock.protoMethodWithoutArgs(); }).toThrow();
-		expect(function() { mock.protoMethodWithArgs(1, 2); }).toThrow();
-		expect(function() { mock.protoMethodOfDerivedClass(); }).toThrow();
+		expect(function () { mock.protoMethodWithoutArgs(); }).toThrow();
+		expect(function () { mock.protoMethodWithArgs(1, 2); }).toThrow();
+		expect(function () { mock.protoMethodOfDerivedClass(); }).toThrow();
 		expect(mock._privateMethod).toBe(undefined);
 	});
 
 	it('should provide default unexpected-call behavior for all "public" instance methods', function () {
 		var mock = a.mock(blueprint);
-		expect(function(){mock.instMethodWithoutArgs();}).toThrow();
-		expect(function(){mock.instMethodWithArgs(1,2);}).toThrow();
+		expect(function () { mock.instMethodWithoutArgs(); }).toThrow();
+		expect(function () { mock.instMethodWithArgs(1, 2); }).toThrow();
 		expect(mock._privateMethod).toBe(undefined);
 	});
 
 	it('should provide default unexpected-call behavior for all "public" instance methods of a derived class', function () {
 		var mock = a.mock(derivedClassBlueprint);
-		expect(function (){mock.instMethodOfDerivedClass();}).toThrow();
+		expect(function () { mock.instMethodOfDerivedClass(); }).toThrow();
 		expect(mock._privateMethod).toBe(undefined);
 	});
 
@@ -66,5 +66,42 @@ describe('jsFake mock', function(){
 		expect(function () { mock.privilegedMethod(); }).toThrow();
 	});
 
-});
+	it('should support mocking of methods with Jasmine', function () {
+		var mock = a.mock(SomeClass);
+		spyOn(mock, 'protoMethodWithoutArgs').andReturn('mocked');
 
+		var result = mock.protoMethodWithoutArgs();
+
+		expect(result).toEqual('mocked');
+		expect(mock.protoMethodWithoutArgs).toHaveBeenCalled();
+	});
+
+	it('should track number of times a method is called with Jasmine', function () {
+		var mock = a.mock(SomeClass);
+		spyOn(mock, 'protoMethodWithoutArgs');
+
+		mock.protoMethodWithoutArgs();
+		mock.protoMethodWithoutArgs();
+
+		expect(mock.protoMethodWithoutArgs.callCount).toBe(2);
+	});
+
+	it('should bind "this" in mocked method with Jasmine', function () {
+		var mock = a.mock(SomeClass);
+		mock.prop = 'value';
+
+		spyOn(mock, 'protoMethodWithoutArgs').andCallFake(function () {
+			return this.prop;
+		});
+
+		expect(mock.protoMethodWithoutArgs()).toEqual('value');
+	});
+
+	it('should fail to mock undefined method with Jasmine', function () {
+		var mock = a.mock(SomeClass);
+		expect(function () {
+			spyOn(mock, 'NONeXISTINGmETHOD');
+		}).toThrow();
+	});
+
+});
